855e0b81c4f8688bef438f3ae4254b68e6d43d22
ArifCemal <bilgisayarif1992@gmail.com>
2020-01-19 00:10:11 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -18,7 +18,7 @@ public class Main {
 	public static void getCarInfo() {
 		collectCarData(source);
 
-		System.out.println("There is "+ map.size() + " car:");
+		System.out.println("There is " + map.size() + " car:");
 		System.out.println();
 		for (int i = 0; i < map.size(); i++) {
 			System.out.println((i + 1) + ". " + map.get(i).toString());
@@ -28,8 +28,8 @@ public class Main {
 			Car bestOne = sortCarPrices(source).get(0);
 
 			System.out.println();
-			System.out.println("The best car offer is: " + bestOne.getPrice() + " TL '" + bestOne.getColor()
-					+ "' colored car.");
+			System.out.println(
+					"The best car offer is: " + bestOne.getPrice() + " TL ('" + bestOne.getColor() + "' colored car)");
 			System.out.println();
 			System.out.println("Price ranking:");
 			System.out.println(sortCarPrices(source).toString());
